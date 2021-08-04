5647a0622a33537d4f461f4e8e65414339d45f39
birkanTonkal <tonkalkamilbirkan@gmail.com>
2021-08-05 02:49:49 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 const menu = () => {
     let menuButton = document.querySelector(".hamburger-menu img");
     let menuPage = document.querySelector(".nav-box-items");
+    let exceptHeader = document.querySelectorAll("section, footer");
     menuButton.addEventListener("click", function() {
-        menuPage.classList.toggle("active");
-        console.log(menuPage);
+        active = menuPage.classList.toggle("active");
+        if (active) {
+            menuButton.src = "images/icon-close.svg";
+            exceptHeader.forEach( (elements) => {
+                elements.style.display = "none";
+            })
+        }
+        else {
+            menuButton.src = "images/icon-hamburger.svg";
+            exceptHeader.forEach( (elements) => {
+                elements.style.display = "block";
+            })            
+        }
+        
+        
     } ) 
+    
 
 }
 
